2c405aef9f487240f026279d2fd2a61cddfd09fe
NathyAlem22 <nathyalem22@gmail.com>
2023-05-25 03:29:37 -0700

    adding files

---
+++ environ.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * _myenva - prints the current environment
+ * @info: Structure containing potential arguments. 
+ * Return: Always 0
+ */
+int _myenva(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenva - gets the value of an environ variable
+ * @info: Structure containing potential arguments.
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenva(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenva - Initialize a new environment variable.
+ * @info: Structure containing potential arguments.
+ *  Return: Always 0
+ */
+int _mysetenva(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenva - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenva(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_enva_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_enva_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
